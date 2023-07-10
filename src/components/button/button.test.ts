aab676d316ad4186cfba4f4af4329a368f2f9926
shareef <creazyhell@gmail.com>
2023-07-10 06:52:39 +0200

    more tests

--- src/components/button/button.test.ts
+++ src/components/button/button.test.ts
@@ -4,62 +4,41 @@ import Button from './Button.vue'
 
 describe('Button', () => {
   it('renders properly', () => {
-    const wrapper = mount(Button)
+    const wrapper = mount(Button, { slots: { default: 'content' } })
     expect(wrapper.exists()).toBeTruthy()
-
     expect(wrapper.classes()).toContain('vex-button')
+    expect(wrapper.text()).toBe('content')
   })
 
   it('emits a click event when clicked without an event handler', async () => {
     const wrapper = mount(Button)
     await wrapper.trigger('click')
-
     expect(wrapper.emitted('click')?.length).toBeTruthy()
   })
 
   it('emits a click event when clicked with an event handler', async () => {
     const wrapper = mount(Button, { props: { onClick: () => {} } })
     await wrapper.trigger('click')
-
     expect(wrapper.emitted('click')?.length).toBeTruthy()
   })
 
-  it('shows a destructive button when using `destructive` prop', () => {
+  it('destructive button', () => {
     const wrapper = mount(Button, { props: { destructive: true } })
     expect(wrapper.classes()).toContain('vex-button-destructive')
   })
 
-  it('disables the button when the `disabled` prop is set', () => {
-    const wrapper = mount(Button, {
-      props: {
-        disabled: true,
-      },
-    })
-
+  it('disabled state', () => {
+    const wrapper = mount(Button, { props: { disabled: true } })
     expect(wrapper.attributes('disabled')).toBe('')
   })
 
-  it('displays a loading spinner when the `loading` prop is set', () => {
-    const wrapper = mount(Button, {
-      props: {
-        loading: true,
-      },
-    })
-
-    expect(wrapper.find('.vex-button-loading').exists()).toBe(true)
+  it('icon-only', () => {
+    const wrapper = mount(Button, { props: { iconOnly: true } })
+    expect(wrapper.classes()).toContain('vex-button-icon-only')
   })
 
-  it('applies the correct classes when the `iconOnly` prop is set', () => {
-    const wrapper = mount(Button, {
-      props: {
-        iconOnly: true,
-      },
-      slots: {
-        default: '<i class="fa fa-plus"></i>',
-      },
-    })
-
-    expect(wrapper.classes()).toContain('vex-button-icon-only')
-    expect(wrapper.find('i.fa.fa-plus').exists()).toBe(true)
+  it('loading state', () => {
+    const button = mount(Button, { props: { loading: true } })
+    expect(button.classes()).toContain('vex-button-loading')
   })
 })
