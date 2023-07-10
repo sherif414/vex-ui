da63d5301fe12506ad5d25bb2dd4025e17732a89
shareef <creazyhell@gmail.com>
2023-07-10 09:57:40 +0200

    refactor code semantics

--- src/components/accordion/AccordionItem.vue
+++ src/components/accordion/AccordionItem.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
-import { ref, inject, computed } from 'vue'
-import type { AccordionInjectedState } from './types'
+import { inject, computed, toRef, toValue } from 'vue'
 import TransitionExpand from './TransitionExpand.vue'
 import { IconArrowDown } from '../icons'
 import { getRandomString } from '@/composables/helpers'
+import { ACCORDION_INJECTION_KEY } from '.'
 
-/**********************************
- * 📌 component meta
- ***********************************/
+//==================================================
+// 📌 component meta
+//==================================================
 
-//TODO: does this custom tag `@required` cause problems?
-const props = withDefaults(
+const p = withDefaults(
   defineProps<{
     /**
      * specifies the content shown while the item is expanded
@@ -24,116 +23,122 @@ const props = withDefaults(
 
     /**
      * whether the item should be initially expanded
-     * @default false
      */
     initiallyExpanded?: boolean
 
     /**
      * expands the item and prevents it from being collapsed
-     * @default false
      */
     alwaysExpanded?: boolean
 
     /**
      * disables the item and prevents interaction with it
-     * @default false
      */
     disabled?: boolean
 
     /**
      * specifies the heading `<h1>, <h2> ..etc` level
-     * @required
+     * @default '3'
      */
-    headingLevel: '1' | '2' | '3' | '4' | '5' | '6'
+    headingLevel?: '1' | '2' | '3' | '4' | '5' | '6'
   }>(),
   {
-    initiallyExpanded: false,
-    disabled: false,
-    alwaysExpanded: false,
+    headingLevel: '3',
   }
 )
 
-const emit = defineEmits(['toggle'])
+const slots = defineSlots<{
+  header(props: { isExpanded: boolean }): any
+  default(props: { isExpanded: boolean }): any
+  iconAppend(props: { isExpanded: boolean }): any
+  iconPrepend(props: { isExpanded: boolean }): any
+}>()
 
-/**********************************
- * 📌 attrs
- ***********************************/
+const emit = defineEmits<{
+  toggle: [expanded: boolean]
+}>()
 
-const heading = computed(() => `h${props.headingLevel}` as const)
-const panelID = `accordion-item-panel-${getRandomString(6)}`
-const buttonID = `accordion-item-controls-${getRandomString(6)}`
+const heading = computed(() => `h${p.headingLevel}` as const)
+const PANEL_ID = `accordion-item-panel-${getRandomString(6)}`
+const TRIGGER_ID = `accordion-item-controls-${getRandomString(6)}`
 
-/**********************************
- * 📌 expand/collapse logic
- ***********************************/
+//==================================================
+// 📌 context
+//==================================================
 
-const { activeIndex, arrow, arrowPosition, getIndex } = inject(
-  'vex-accordion-injected-state'
-) as AccordionInjectedState
+const ctx = inject(ACCORDION_INJECTION_KEY, null)
 
-const index = getIndex()
+if (!ctx) {
+  throw new Error('[vex] <AccordionItem> is missing an <Accordion> parent component.')
+}
 
-if (props.initiallyExpanded) {
+const hideArrow = toRef(ctx.arrow)
+const arrowPosition = toRef(ctx.arrowPosition)
+const activeIndex = toRef(ctx.activeIndex)
+const index = toValue(ctx.getIndex())
+
+//==================================================
+// 📌 expand/collapse
+//==================================================
+
+if (p.initiallyExpanded) {
   activeIndex.value instanceof Set
     ? activeIndex.value.add(index)
     : (activeIndex.value = index)
 }
 
-/** whether this accordion item index is active */
-const isActive = computed<boolean>(() => {
-  if (activeIndex.value instanceof Set) {
-    return activeIndex.value.has(index)
-  }
+const isActive = computed<boolean>(() =>
+  activeIndex.value instanceof Set
+    ? activeIndex.value.has(index)
+    : activeIndex.value === index
+)
 
-  return activeIndex.value === index
-})
+const isExpanded = computed<boolean>(() => p.alwaysExpanded || isActive.value)
 
-const isExpanded = computed<boolean>(() => props.alwaysExpanded || isActive.value)
-
-function handleClick() {
-  if (props.alwaysExpanded) return
-
-  emit('toggle')
+function onToggle() {
+  if (p.alwaysExpanded) return
 
   if (activeIndex.value instanceof Set) {
     isActive.value ? activeIndex.value.delete(index) : activeIndex.value.add(index)
-    return
+  } else {
+    activeIndex.value = isActive.value ? undefined : index
   }
-
-  activeIndex.value = isActive.value ? undefined : index
+  emit('toggle', isExpanded.value)
 }
 </script>
 
 <template>
-  <div :class="['vex-accordion-item', { 'vex-accordion-item-open': isExpanded }]">
+  <div :class="['vex-accordion-item', { '--open': isExpanded }]">
     <!-- header -->
 
-    <Component class="vex-accordion-item-header" :is="heading">
+    <Component :is="heading" class="vex-accordion-item-header">
       <button
         type="button"
-        class="vex-accordion-item-button"
-        @click="handleClick"
+        class="vex-accordion-item-header-button"
+        @click="onToggle"
         :aria-expanded="isExpanded"
-        :aria-disabled="props.alwaysExpanded || props.disabled"
-        :disabled="props.disabled"
-        :aria-controls="panelID"
-        :id="buttonID"
+        :aria-disabled="p.alwaysExpanded || p.disabled"
+        :disabled="p.disabled"
+        :aria-controls="PANEL_ID"
+        :id="TRIGGER_ID"
       >
         <slot name="iconPrepend" :isExpanded="isExpanded">
           <IconArrowDown
-            v-if="arrowPosition === 'start' && arrow"
-            class="vex-accordion-item-icon"
+            v-if="arrowPosition === 'start' && !hideArrow"
+            class="vex-accordion-item-header-chevron"
+            aria-hidden="true"
           />
         </slot>
-        <span>
+        <span class="vex-accordion-item-header-button-content">
           <slot name="header" :isExpanded="isExpanded">
-            {{ props.header }}
+            {{ p.header }}
           </slot>
         </span>
         <slot name="iconAppend" :isExpanded="isExpanded">
           <IconArrowDown
-            v-if="arrowPosition === 'end' && arrow"
-            class="vex-accordion-item-icon"
+            v-if="arrowPosition === 'end' && !hideArrow"
+            class="vex-accordion-item-header-chevron"
+            aria-hidden="true"
           />
         </slot>
       </button>
@@ -146,12 +151,12 @@ function handleClick() {
         v-show.lazy="isExpanded"
         class="vex-accordion-item-content"
         role="region"
-        :id="panelID"
-        :aria-labelledby="buttonID"
+        :id="PANEL_ID"
+        :aria-labelledby="TRIGGER_ID"
       >
-        <div>
-          <slot>
-            {{ props.content }}
+        <div class="vex-accordion-item-content-wrapper">
+          <slot :is-expanded="isExpanded">
+            {{ p.content }}
           </slot>
         </div>
       </div>
@@ -165,45 +170,38 @@ function handleClick() {
   transition-timing-function: var(--vex-transition-easing);
   transition-duration: 150ms;
   border: 1px solid transparent;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  border-radius: var(--vex-border-radius-sm);
+  gap: 0;
+}
 
-  &-header {
-    all: unset;
-    margin: 0;
-    font-weight: normal;
-    font-size: inherit;
-  }
+//------ header ------//
 
-  &-content {
-    will-change: height;
-
-    & > div {
-      padding: var(--vex-spacing-1) var(--vex-spacing-4) var(--vex-spacing-3)
-        var(--vex-spacing-4);
-    }
-  }
+.vex-accordion-item-header {
+  font-weight: inherit;
+  font-family: inherit;
+  font-size: var(--vex-font-size-sm);
+  border-radius: var(--vex-border-radius-sm);
+  margin: 0;
 
   &-button {
-    all: unset;
-    color: inherit;
-    font-family: inherit;
-    -webkit-appearance: none;
-    appearance: none;
-    border-radius: 4px;
     display: flex;
     align-items: center;
     gap: var(--vex-spacing-4);
     width: 100%;
     padding: var(--vex-spacing-3) var(--vex-spacing-4);
-    outline: 1px solid transparent;
-    border-style: none;
+    color: inherit;
+    font-family: inherit;
+    font-size: inherit;
+    font-weight: inherit;
+    appearance: none;
+    border-radius: inherit;
+    border: none;
     cursor: pointer;
     background-color: transparent;
-    box-sizing: border-box;
     text-align: start;
-
-    > span {
-      flex-grow: 1;
-    }
 
     &:disabled {
       cursor: not-allowed;
@@ -211,21 +209,35 @@ function handleClick() {
     }
 
     &:focus-visible {
-      outline-color: var(--vex-clr-primary-400);
+      outline: 1px solid var(--vex-clr-primary-400);
+    }
+
+    &-content {
+      flex-grow: 1;
     }
   }
 
-  &-icon {
-    transition-property: transform;
-    transition-timing-function: var(--vex-transition-easing);
-    transition-duration: 300ms;
+  &-chevron {
+    transition: transform 300ms var(--vex-transition-easing);
     width: 16px;
     height: 16px;
     flex-shrink: 0;
 
-    .vex-accordion-item-open & {
+    .vex-accordion-item.--open & {
       transform: rotate(180deg);
     }
+  }
+}
+
+//------ content ------//
+
+.vex-accordion-item-content {
+  will-change: height;
+  font-size: var(--vex-font-size-sm);
+
+  &-wrapper {
+    padding: var(--vex-spacing-1) var(--vex-spacing-4) var(--vex-spacing-3)
+      var(--vex-spacing-4);
   }
 }
 </style>
--- src/components/accordion/accordion.vue
+++ src/components/accordion/accordion.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
 import { provide, ref, toRef, watch } from 'vue'
-import type { ActiveIndex, AccordionInjectedState } from './types'
 import { useListNavigation } from '@/composables'
+import { ACCORDION_INJECTION_KEY, type ActiveIndex } from '.'
 
-/**********************************
- * 📌 component meta
- ***********************************/
+//==================================================
+// 📌 component meta
+//==================================================
 
-const props = withDefaults(
+const p = withDefaults(
   defineProps<{
     /**
      * specifies the accordion variant, which changes how the accordion looks
      * @default 'default'
      */
-    variant?: 'outline' | 'ladder' | 'default' | 'filled'
+    variant?: 'outline' | 'ladder' | 'default' | 'light'
 
     /**
      * specifies the chevron arrow position
@@ -22,51 +22,45 @@ const props = withDefaults(
     arrowPosition?: 'start' | 'end'
 
     /**
-     * whether to show or hide the chevron arrow
+     * whether to hide the chevron arrow
      * @default true
      */
-    arrow?: boolean
+    hideArrow?: boolean
 
     /**
      * whether to allow multiple active accordion-items at the same time or not
-     * @default false
      */
     multiple?: boolean
 
     /**
      * whether to loop back to the other end of the list upon reaching the end,
      * only relevant when navigating with keyboard
-     * @default true
      */
     loop?: boolean
   }>(),
   {
     variant: 'default',
-    loop: true,
     arrowPosition: 'end',
-    arrow: true,
-    multiple: false,
   }
 )
 
-/**********************************
- * 📌 keyboard navigation
- ***********************************/
+const CHILDREN_SELECTOR = '.vex-accordion-item-button:enabled'
 
-const { eventListener } = useListNavigation(
-  '.vex-accordion-item-button:enabled',
-  props.loop
-)
+//==================================================
+// 📌 keyboard & focus management
+//==================================================
 
-/**********************************
- * 📌 provide context to children
- ***********************************/
+const { eventListener: onKeydown } = useListNavigation(CHILDREN_SELECTOR, p.loop)
+
+//==================================================
+// 📌 context provider
+//==================================================
 
 let indexCount = 0
-const activeIndex = ref<ActiveIndex>(props.multiple ? new Set() : undefined)
+const activeIndex = ref<ActiveIndex>(p.multiple ? new Set() : undefined)
 
 watch(
-  () => props.multiple,
+  () => p.multiple,
   (value) => {
     activeIndex.value = value ? new Set() : undefined
   }
@@ -76,19 +70,16 @@ function getIndex() {
   return ++indexCount
 }
 
-provide<AccordionInjectedState>('vex-accordion-injected-state', {
+provide(ACCORDION_INJECTION_KEY, {
   activeIndex,
   getIndex,
-  arrow: toRef(props, 'arrow'),
-  arrowPosition: toRef(props, 'arrowPosition'),
+  arrow: () => p.hideArrow,
+  arrowPosition: () => p.arrowPosition,
 })
 </script>
 
 <template>
-  <div
-    :class="['vex-accordion', `vex-accordion-variant-${props.variant}`]"
-    @keydown="eventListener"
-  >
+  <div :class="['vex-accordion', `--variant-${p.variant}`]" @keydown="onKeydown">
     <slot />
   </div>
 </template>
@@ -98,58 +89,66 @@ provide<AccordionInjectedState>('vex-accordion-injected-state', {
   width: 100%;
   display: flex;
   flex-direction: column;
-  font-size: var(--vex-font-size-sm);
   color: var(--vex-clr-neutral-900);
+  border-radius: var(--vex-border-radius-sm);
+}
 
-  &-variant-default {
-    gap: var(--vex-spacing-1);
-    .vex-accordion-item-button:enabled:hover {
+//------ variant default ------//
+
+.vex-accordion.--variant-default {
+  gap: var(--vex-spacing-1);
+
+  .vex-accordion-item-header-button:enabled:hover {
+    background-color: var(--vex-clr-neutral-100);
+  }
+  .vex-accordion-item.--open {
+    background-color: var(--vex-clr-neutral-100);
+  }
+}
+
+//------ variant ladder ------//
+
+.vex-accordion.--variant-ladder {
+  .vex-accordion-item {
+    border-bottom-color: var(--vex-clr-neutral-200);
+
+    &.--open {
       background-color: var(--vex-clr-neutral-100);
     }
-    .vex-accordion-item-open {
-      background-color: var(--vex-clr-neutral-100) !important;
-    }
-  }
 
-  &-variant-ladder {
-    .vex-accordion-item {
-      border-bottom-color: var(--vex-clr-neutral-200);
-
-      &-open {
-        background-color: var(--vex-clr-neutral-100) !important;
-      }
-
-      &-button:enabled:hover {
-        background-color: var(--vex-clr-neutral-100);
-      }
-    }
-  }
-
-  &-variant-outline {
-    border-radius: 8px;
-    border: 1px solid var(--vex-clr-neutral-200);
-
-    .vex-accordion-item ~ .vex-accordion-item {
-      border-top-color: var(--vex-clr-neutral-200);
-    }
-    .vex-accordion-item-button:enabled:hover {
-      background-color: var(--vex-clr-neutral-100);
-    }
-    .vex-accordion-item-open {
+    &-header-button:enabled:hover {
       background-color: var(--vex-clr-neutral-100);
     }
   }
+}
 
-  &-variant-filled {
-    gap: var(--vex-spacing-2);
-    .vex-accordion-item {
-      background-color: var(--vex-clr-neutral-50);
-      border-radius: var(--vex-border-radius-md);
-    }
-    .vex-accordion-item-open {
-      background-color: white !important;
-      border-color: rgba(209, 213, 219);
-    }
+//------ variant outline ------//
+
+.vex-accordion.--variant-outline {
+  border: 1px solid var(--vex-clr-neutral-200);
+
+  .vex-accordion-item ~ .vex-accordion-item {
+    border-top-color: var(--vex-clr-neutral-200);
+  }
+  .vex-accordion-item-header-button:enabled:hover {
+    background-color: var(--vex-clr-neutral-100);
+  }
+  .vex-accordion-item.--open {
+    background-color: var(--vex-clr-neutral-100);
+  }
+}
+
+//------ variant light ------//
+
+.vex-accordion.--variant-light {
+  gap: var(--vex-spacing-2);
+
+  .vex-accordion-item {
+    background-color: var(--vex-clr-neutral-50);
+  }
+  .vex-accordion-item.--open {
+    background-color: white;
+    border-color: rgba(209, 213, 219);
   }
 }
 </style>
