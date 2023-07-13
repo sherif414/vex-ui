41f23cab4e37899303c5b7ee142e1a02288fb78d
shareef <creazyhell@gmail.com>
2023-07-13 16:41:18 +0200

    new composable focus-trap

--- src/composables/focus-trap.ts
+++ src/composables/focus-trap.ts
@@ -1,19 +1,14 @@
+import type { RefOrGetter } from '@/types'
 import type { FocusTrap } from 'focus-trap'
 import { createFocusTrap, type Options } from 'focus-trap'
-import { type MaybeRefOrGetter, watchEffect, toRef, onUnmounted } from 'vue'
+import { type MaybeRefOrGetter, watchEffect, toRef, onScopeDispose, watch } from 'vue'
 
 export function useFocusTrap(
   el: MaybeRefOrGetter<HTMLElement | null | undefined>,
-  active?: MaybeRefOrGetter<boolean>,
+  active?: RefOrGetter<boolean> | null,
   options: Options = {}
-): {
-  activate: () => void
-  deactivate: () => void
-  pause: () => void
-  resume: () => void
-} {
+) {
   const target = toRef(el)
-  const isActive = active === undefined ? null : toRef(active)
   let trap: FocusTrap | null = null
 
   watchEffect(() => {
@@ -23,13 +18,13 @@ export function useFocusTrap(
   })
 
   // implicitly switch to manual mode if no `active` arg was passed
-  if (isActive) {
-    watchEffect(() => {
-      isActive.value ? trap?.activate() : trap?.deactivate()
+  if (active) {
+    watch(active, (val) => {
+      val ? trap?.activate() : trap?.deactivate()
     })
   }
 
-  onUnmounted(() => {
+  onScopeDispose(() => {
     trap?.deactivate()
     trap = null
   })
