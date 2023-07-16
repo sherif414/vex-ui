96113d1bcd1cb327eaf75fe0a53ce7442b8d7c73
shareef <creazyhell@gmail.com>
2023-07-16 20:07:03 +0200

    remove unused code

--- src/composables/helpers.ts
+++ src/composables/helpers.ts
@@ -1,25 +1,3 @@
-export const isEmpty = (value: unknown): boolean => {
-  if (value === null || value === undefined || value === '') return true
-
-  return Array.isArray(value) && value.length === 0
-}
-
-export const isNullOrUndefined = (value: unknown): value is undefined | null => {
-  return value === null || value === undefined
-}
-
-export const isEmptyArray = (arr: unknown): boolean => {
-  return Array.isArray(arr) && arr.length === 0
-}
-
-export const isObject = (obj: unknown): obj is Record<string, unknown> =>
-  obj !== null && !!obj && typeof obj === 'object' && !Array.isArray(obj)
-
-export const isNumeric = (value: unknown): value is number =>
-  (typeof value === 'string' || typeof value === 'number') &&
-  value !== '' &&
-  !isNaN(Number(value))
-
 export const isString = (value: unknown): value is string => typeof value === 'string'
 
 export function getRandomString(length: number): string {
