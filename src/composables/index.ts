47ee0ef26c1237ae2f6ce32dba0caaabdf4d7003
shareef <creazyhell@gmail.com>
2023-07-23 06:08:42 +0200

    new composable dismissable layer

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -3,6 +3,8 @@ export { useListNavigation } from './listNavigation'
 export { useListSelection } from './list-selection'
 export { useFocusTrap } from './focus-trap'
 export { useID } from './use-id'
+export * from './dismissible-layer'
+export * from './remove-body-scroll'
 export * from './floating'
 
 // remove me
