80f5763fc294ddc9a707cf712f89ebb705a0e9f0
shareef <creazyhell@gmail.com>
2023-07-23 06:08:54 +0200

    new composable remove body scroll

---
+++ src/composables/remove-body-scroll.ts
@@ -0,0 +1,29 @@
+import { nextTick } from 'vue'
+
+let isRemoved = false
+
+export function useRemoveBodyScroll() {
+  function removeBodyScroll(): void {
+    if (isRemoved) return
+    nextTick(() => {
+      let scrollBarWidth = window.innerWidth - document.documentElement.clientWidth
+      document.body.style.marginRight = `${scrollBarWidth}px`
+      document.body.style.overflow = 'hidden'
+      isRemoved = true
+    })
+  }
+
+  function returnBodyScroll(): void {
+    if (!isRemoved) return
+    nextTick(() => {
+      document.body.style.marginRight = ''
+      document.body.style.overflow = ''
+      isRemoved = false
+    })
+  }
+
+  return {
+    remove: removeBodyScroll,
+    unRemove: returnBodyScroll,
+  }
+}
