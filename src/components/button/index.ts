30d0bdef121b2c459be2f3f35d5721d376659d42
shareef <creazyhell@gmail.com>
2023-07-24 07:14:41 +0200

    fix imports

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,2 +1,2 @@
 export { default as Button } from './Button.vue'
-import './style/index.scss'
+import './Button.scss'
--- src/components/dialog/index.ts
+++ src/components/dialog/index.ts
@@ -1,6 +1,6 @@
 import { reactive } from 'vue'
 export { default as Dialog } from './Dialog.vue'
-import './style/index.scss'
+import './Dialog.scss'
 
 export const dialogStore = reactive({
   openDialogsCount: 0,
