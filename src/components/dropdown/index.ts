939f5812d0d4206e6333250989b6549879c3a807
shareef <creazyhell@gmail.com>
2023-07-24 06:43:59 +0200

    revert folder changes

--- src/components/dropdown/index.ts
+++ src/components/dropdown/index.ts
@@ -1,2 +1,4 @@
-export * from './src'
-import './style/index.scss'
+export { default as Dropdown } from './Dropdown.vue'
+export { default as DropdownItem } from './DropdownItem.vue'
+import './Dropdown.scss'
+import './DropdownItem.scss'
