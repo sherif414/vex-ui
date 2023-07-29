934eb8f60494a1278ff445169fadcb6959b5adf7
shareef <creazyhell@gmail.com>
2023-07-29 08:23:34 +0200

    more types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,11 @@
 import type { Ref } from 'vue'
 
+export type Fn = () => void
+
 export type Getter<T> = () => T
+export type Setter<T> = (value: T) => T
+
+export type MaybeGetter<T> = Getter<T> | T
+export type MaybeSetter<T> = Setter<T> | T
+
 export type RefOrGetter<T> = Ref<T> | Getter<T>
