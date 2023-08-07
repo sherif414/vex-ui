0fe840e3c071c6cdd8f50d5217d19a0330ab4d55
shareef <creazyhell@gmail.com>
2023-08-07 14:22:41 +0200

    new composable

---
+++ src/composables/dropdown.ts
@@ -0,0 +1,49 @@
+import type { MaybeRefOrGetter, RefOrGetter, Getter } from '@/types'
+import { toRef, toValue, watch } from 'vue'
+
+interface UseDropdownOptions {
+  role?: 'menu' | 'listbox'
+  dropdownId?: string
+  triggerId?: string
+  ariaExpanded?: MaybeRefOrGetter<boolean>
+  ariaActiveDescendant: Getter<string>
+}
+
+/**
+ * handles setting aria attributes
+ */
+export function useDropdownAria(
+  trigger: Getter<HTMLElement | null>,
+  dropdown: Getter<HTMLElement | null>,
+  options: UseDropdownOptions
+) {
+  //----------------------------------------------------------------------------------------------------
+  // 📌 attributes
+  //----------------------------------------------------------------------------------------------------
+
+  const { ariaActiveDescendant, dropdownId, triggerId, role } = options
+  const isExpanded = toRef(options.ariaExpanded)
+
+  watch(trigger, (el) => {
+    if (!el) return
+    el.setAttribute('aria-expanded', `${isExpanded.value}`)
+    el.setAttribute('aria-controls', `${dropdownId}`)
+    el.setAttribute('aria-haspopup', `${role}`)
+    el.setAttribute('id', `${triggerId}`)
+  })
+
+  watch(dropdown, (el) => {
+    if (!el) return
+    el.setAttribute('aria-labelledby', `${triggerId}`)
+    el.setAttribute('role', `${role}`)
+    el.setAttribute('id', `${dropdownId}`)
+  })
+
+  watch(isExpanded, (expanded) => {
+    trigger()?.setAttribute('aria-expanded', `${expanded}`)
+  })
+
+  watch(ariaActiveDescendant, (active) => {
+    dropdown()?.setAttribute('aria-activedescendant', `${active}`)
+  })
+}
