e9ceb6545fd2a56b08d92e856370ef04a11047aa
shareef <creazyhell@gmail.com>
2023-08-07 12:47:00 +0200

    new helpers

--- src/composables/helpers.ts
+++ src/composables/helpers.ts
@@ -1,4 +1,26 @@
+import { EXPOSED_EL } from '@/config'
+import type { ComponentPublicInstance } from 'vue'
+
+//----------------------------------------------------------------------------------------------------
+// 📌 validators
+//----------------------------------------------------------------------------------------------------
+
+export const isClient = typeof window !== 'undefined'
 export const isString = (value: unknown): value is string => typeof value === 'string'
+export const isFunction = (value: unknown): value is Function => value instanceof Function
+export const isIOS = /*#__PURE__*/ getIsIOS()
+
+//----------------------------------------------------------------------------------------------------
+// 📌 getters
+//----------------------------------------------------------------------------------------------------
+
+export function getIsIOS() {
+  return (
+    isClient &&
+    /*#__PURE__*/ window?.navigator?.userAgent &&
+    /*#__PURE__*/ /iP(ad|hone|od)/.test(/*#__PURE__*/ window.navigator.userAgent)
+  )
+}
 
 export function getRandomString(length: number): string {
   let result = ''
@@ -8,3 +30,21 @@ export function getRandomString(length: number): string {
   }
   return result
 }
+
+export function getElementFromRef(
+  vm: ComponentPublicInstance | Element | null,
+  component: string
+): HTMLElement | null {
+  if (vm == null) return null
+  if (vm instanceof Element) return vm as HTMLElement
+  if (EXPOSED_EL in vm && vm[EXPOSED_EL] instanceof Element) return vm[EXPOSED_EL] as HTMLElement
+  if (vm.$el instanceof Element) return vm.$el as HTMLElement
+
+  throw new Error(`[vex] <${component}> received a non Element root child`)
+}
+
+//----------------------------------------------------------------------------------------------------
+// 📌 specials
+//----------------------------------------------------------------------------------------------------
+
+export const noop = () => {}
