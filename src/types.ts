21ecaaf3365f8e960c78226c4b7d08adc044eab7
shareef <creazyhell@gmail.com>
2023-08-08 17:09:37 +0200

    refactor types

--- src/types.ts
+++ src/types.ts
@@ -2,11 +2,17 @@ import type { Ref } from 'vue'
 
 export type Fn = () => void
 
-export type Getter<T> = () => T
+// signal reactivity types
+export type Get<T> = () => T
+export type Getter<T> = (compute?: (v: T) => T) => T
+
+export type Set<T> = (value: T) => void
+export type Setter<T> = Set<((v: T) => T) | T>
+
+export type Signal<T> = [Getter<T>, Setter<T>]
+
+// vue reactivity types
+export type MaybeRef<T> = Ref<T> | T
 export type MaybeGetter<T> = Getter<T> | T
 export type RefOrGetter<T> = Ref<T> | Getter<T>
 export type MaybeRefOrGetter<T> = RefOrGetter<T> | T
-
-export type Setter<T> = (value: ((v: T) => T) | T) => void
-
-export type Signal<T> = [Getter<T>, Setter<T>]
