0697df6c28a9d0d0b444e4eb77f9d9334441e7db
shareef <creazyhell@gmail.com>
2023-08-08 17:14:55 +0200

    rename params

--- src/composables/signal.ts
+++ src/composables/signal.ts
@@ -2,8 +2,8 @@ import type { Signal } from '@/types'
 import { shallowRef } from 'vue'
 import { isFunction } from './helpers'
 
-export function useSignal<T>(value: T): Signal<T> {
-  const r = shallowRef(value)
+export function useSignal<T>(initial: T): Signal<T> {
+  const r = shallowRef(initial)
 
   // make sure to use named function so they show up in devtools
   return [
