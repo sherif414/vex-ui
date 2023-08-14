404fa94c59e62e35a2bec0d155df0eef3c2e4663
shareef <creazyhell@gmail.com>
2023-08-14 06:16:48 +0200

    added options

--- src/composables/keydown.ts
+++ src/composables/keydown.ts
@@ -1,9 +1,13 @@
-import type { Getter, KeyIntent, NavigationKeys } from '@/types'
+import type { Getter, KeyIntent, NavigationKeys, Orientation } from '@/types'
 import { useEventListener } from '@vueuse/core'
 import { getKeyIntent } from './helpers'
 
 type keydownHandler = (e: KeyboardEvent, intent: KeyIntent, key: NavigationKeys) => void
 type Stop = () => void
+type UseKeydownIntentOptions = {
+  orientation?: Getter<Orientation>
+}
+
 const VALID_KEYS = [
   'ArrowDown',
   'ArrowUp',
@@ -16,12 +20,16 @@ const VALID_KEYS = [
   'Escape',
 ] as const
 
-export function useKeydownIntent(target: Getter<HTMLElement | null>, cb: keydownHandler): Stop {
+export function useKeydownIntent(
+  target: Getter<HTMLElement | null>,
+  handler: keydownHandler,
+  options: UseKeydownIntentOptions = {}
+): Stop {
   return useEventListener(target, 'keydown', (e: KeyboardEvent) => {
     const key = e.key as NavigationKeys
     if (!VALID_KEYS.includes(key)) return
 
-    const intent = getKeyIntent(key)
-    cb(e, intent, key)
+    const intent = getKeyIntent(key, options.orientation?.())
+    handler(e, intent, key)
   })
 }
