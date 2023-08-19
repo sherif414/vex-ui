bc3051d074011ab40e1aaee863fccfcc3c95e114
shareef <creazyhell@gmail.com>
2023-08-19 09:42:04 +0200

    added toSignal utility

--- src/composables/signal.ts
+++ src/composables/signal.ts
@@ -1,16 +1,20 @@
 import type { Signal } from '@/types'
-import { shallowRef } from 'vue'
+import { shallowRef, type Ref } from 'vue'
 import { isFunction } from './helpers'
 
 export function useSignal<T>(initial: T): Signal<T> {
   const r = shallowRef(initial)
 
+  return toSignal(r)
+}
+
+export function toSignal<T>(v: Ref<T>): Signal<T> {
   return [
     function getter<U>(fn?: (v: T) => U): T | U {
-      return isFunction(fn) ? fn(r.value) : r.value
+      return isFunction(fn) ? fn(v.value) : v.value
     },
     function setter(fn: ((v: T) => T) | T): void {
-      r.value = isFunction(fn) ? fn(r.value) : fn
+      v.value = isFunction(fn) ? fn(v.value) : fn
     },
   ]
 }
