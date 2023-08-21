4abb7dca32c0cd12edbb666187e1cd6166fdfc60
shareef <creazyhell@gmail.com>
2023-08-21 15:06:04 +0200

    better types

--- src/composables/v-model.ts
+++ src/composables/v-model.ts
@@ -1,17 +1,16 @@
-import type { ComputableGetter, ComputableSetter, Signal } from '@/types'
+import type { ComputableGetter, ComputableSetter, Getter, Signal } from '@/types'
 import { getCurrentInstance } from 'vue'
 import { isFunction } from './helpers'
 
-export function useVModel<T>(
-  prop: ComputableGetter<T>,
-  event: string = 'update:modelValue'
-): Signal<T> {
+export function useVModel<T>(prop: Getter<T>, event: string = 'update:modelValue'): Signal<T> {
   const vm = getCurrentInstance()
 
-  return [
-    prop,
-    (val: T | ((v: T) => T)) => {
-      vm?.emit(event, isFunction(val) ? val(prop()) : val)
-    },
-  ]
+  const getter = <U>(fn: (v: T) => U) => {
+    return isFunction(fn) ? fn(prop()) : prop()
+  }
+  const setter = (val: T | ((v: T) => T)) => {
+    vm?.emit(event, isFunction(val) ? val(prop()) : val)
+  }
+
+  return [getter as ComputableGetter<T>, setter as ComputableSetter<T>]
 }
