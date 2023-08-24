2c31d011c19806487552deee8e6d34b61fd350b4
shareef <creazyhell@gmail.com>
2023-08-24 20:35:33 +0200

    revert functional changes

--- src/composables/computed.ts
+++ src/composables/computed.ts
@@ -1,62 +1,26 @@
-// ported from @vueuse/core - computedWithControl
-
-import type { ComputableGetter, Fn, Getter, Signal } from '@/types'
-import type {
-  ComputedGetter,
-  ComputedRef,
-  Ref,
-  WatchOptions,
-  WatchSource,
-  WritableComputedOptions,
-  WritableComputedRef,
-} from 'vue'
-import { computed, customRef, ref, watch } from 'vue'
+import type { Fn } from '@/types'
+import type { ComputedGetter, Ref, WatchOptions, WatchSource, WritableComputedOptions } from 'vue'
+import { computed, customRef, watch } from 'vue'
 import { isFunction } from './helpers'
-import { toSignal } from './signal'
 
-export function useComputed<T>(
-  fn: ComputedGetter<T>,
-  source?: WatchSource | WatchSource[],
-  options?: WatchOptions
-): Getter<T>
-
-export function useComputed<T>(
-  fn: WritableComputedOptions<T>,
-  source?: WatchSource | WatchSource[],
-  options?: WatchOptions
-): Signal<T>
-
-/**
- * You may explicitly define the deps of computed.
- *
- * @param fn
- * @param source
- * @param options
- */
 export function useComputed<T>(
   fn: ComputedGetter<T> | WritableComputedOptions<T>,
-  source?: WatchSource | WatchSource[],
+  source?: WatchSource[],
   options: WatchOptions = {
     flush: 'sync',
   }
-) {
+): Ref<T> {
   if (!source) {
-    if (isFunction(fn)) {
-      const _computed = computed<T>(fn)
-      return () => _computed.value
-    }
-
-    const _computed = computed<T>(fn)
-    return toSignal(_computed)
+    return computed(fn as any)
   }
 
   let v: T = undefined!
   let track: Fn
   let trigger: Fn
-  const dirty = ref(true)
+  let dirty = true
 
   const update = () => {
-    dirty.value = true
+    dirty = true
     trigger()
   }
 
@@ -65,15 +29,15 @@ export function useComputed<T>(
   const getter = !isFunction(fn) ? fn.get : fn
   const setter = !isFunction(fn) ? fn.set : undefined
 
-  const _computed = customRef<T>((_track, _trigger) => {
+  return customRef<T>((_track, _trigger) => {
     track = _track
     trigger = _trigger
 
     return {
       get() {
-        if (dirty.value) {
+        if (dirty) {
           v = getter()
-          dirty.value = false
+          dirty = false
         }
         track()
         return v
@@ -82,11 +46,5 @@ export function useComputed<T>(
         setter?.(v)
       },
     }
-  }) as WritableComputedRef<T>
-
-  if (!isFunction(setter)) {
-    return () => _computed.value as Getter<T>
-  }
-
-  return toSignal(_computed)
+  })
 }
