c2f15639549c8ac878a7d9bc9e5a25cf866a2e1c
shareef <creazyhell@gmail.com>
2023-08-26 09:27:07 +0200

    add emit fallbacks

--- src/composables/v-model.ts
+++ src/composables/v-model.ts
@@ -4,9 +4,10 @@ import { useComputed } from './computed'
 
 export function useVModel<T>(prop: Getter<T>, eventName: string = 'update:modelValue') {
   const vm = getCurrentInstance()
+  const emit = vm?.emit || (vm as any)?.$emit?.bind(vm) || vm?.proxy?.$emit?.bind(vm?.proxy)
 
   return useComputed({
     get: prop,
-    set: (newValue) => vm?.emit(eventName, newValue),
+    set: (newValue) => emit(eventName, newValue),
   })
 }
