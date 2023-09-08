102e1fb72a63da560f34e29e885734aca9b6b547
shareef <creazyhell@gmail.com>
2023-09-08 15:33:45 +0200

    added deselect abstract method

--- src/composables/selection-scope.ts
+++ src/composables/selection-scope.ts
@@ -11,9 +11,16 @@ export abstract class SelectionStrategy<T> {
    * Selects the given value and updates the selected array.
    * @param value - The value to be selected.
    * @param selected - The array of selected values.
-   * @param shouldDeselect - Whether the value should be deselected if already selected.
+   * @param deselectOnReselect - Whether the value should be deselected if already selected.
    */
-  abstract select(value: T, selected: Ref<T[]>, shouldDeselect: boolean): void
+  abstract select(value: T, selected: Ref<T[]>, deselectOnReselect: boolean): void
+
+  /**
+   * Deselects the given value and updates the selected array.
+   * @param value - The value to be deselected.
+   * @param selected - The array of selected values.
+   */
+  abstract deselect(selected: Ref<T[]>, value: T): void
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -26,7 +33,7 @@ export abstract class SelectionStrategy<T> {
 export class SelectionGroup<T extends PrimitiveValue> {
   deselection = () => false
   multiselect = () => false
-  strategy: SelectionStrategy<T> = new SingleSelection()
+  strategy: SelectionStrategy<T> = new SingleSelect()
 
   constructor(
     public selected: Ref<T[]>,
@@ -42,23 +49,20 @@ export class SelectionGroup<T extends PrimitiveValue> {
     watch(
       () => this.multiselect(),
       (multi) => {
-        this.strategy = multi ? new MultiSelection() : new SingleSelection()
+        this.strategy = multi ? new MultiSelect() : new SingleSelect()
       },
       { immediate: true }
     )
   }
 
   /**
-   * Selects the given value using the current selection mode.
-   * @param value - The value to be selected.
+   * Selects or deselects the given value using the current selection mode.
+   * @param value - The value to be selected or deselected.
    */
   select(value: T): void {
     this.strategy.select(value, this.selected, this.deselection())
   }
 
-  /**
-   * clears the selected values.
-   */
   clearSelected(): void {
     this.selected.value = []
   }
@@ -66,28 +70,36 @@ export class SelectionGroup<T extends PrimitiveValue> {
 
 //===
 
-export class SingleSelection<T> extends SelectionStrategy<T> {
+export class SingleSelect<T> extends SelectionStrategy<T> {
   select(value: T, selected: Ref<T[]>, shouldDeselect: boolean): void {
     const isSelected = selected.value.includes(value)
 
     if (isSelected && shouldDeselect) {
-      selected.value = []
+      this.deselect(selected)
     } else if (!isSelected) {
       selected.value = [value]
     }
+  }
+
+  deselect(selected: Ref<T[]>): void {
+    selected.value = []
   }
 }
 
 //===
 
-export class MultiSelection<T> extends SelectionStrategy<T> {
+export class MultiSelect<T> extends SelectionStrategy<T> {
   select(value: T, selected: Ref<T[]>, shouldDeselect: boolean): void {
     const isSelected = selected.value.includes(value)
 
     if (isSelected && shouldDeselect) {
-      selected.value = selected.value.filter((_value) => _value !== value)
+      this.deselect(selected, value)
     } else if (!isSelected) {
       selected.value = [...selected.value, value]
     }
+  }
+
+  deselect(selected: Ref<T[]>, value: T): void {
+    selected.value = selected.value.filter((_value) => _value !== value)
   }
 }
