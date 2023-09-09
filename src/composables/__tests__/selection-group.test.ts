e359e21c0ec4c9ca75b33f5900af7d4035582b97
shareef <creazyhell@gmail.com>
2023-09-09 08:39:34 +0200

    rename

--- src/composables/__tests__/selection-group.test.ts
+++ src/composables/__tests__/selection-group.test.ts
@@ -1,5 +1,5 @@
 import { ref } from 'vue'
-import { MultiSelect, SelectionGroup, SingleSelect } from '../selection-scope'
+import { MultiSelect, useSelectionGroup, SingleSelect } from '../selection-group'
 import { describe, it, expect } from 'vitest'
 
 //----------------------------------------------------------------------------------------------------
@@ -48,46 +48,34 @@ describe('SingleSelect', () => {
 //----------------------------------------------------------------------------------------------------
 
 describe('MultiSelect', () => {
-  it('should handle selecting an already selected value when deselect is true', () => {
+  it('should handle selecting value', () => {
     const selected = ref(['test1'])
     const multiSelect = new MultiSelect<string>(selected)
-    multiSelect.select('test1', true)
-    expect(selected.value).toEqual([])
-  })
-
-  it('should handle selecting an already selected value when deselect is false', () => {
-    const selected = ref(['test1'])
-    const multiSelect = new MultiSelect<string>(selected)
-    multiSelect.select('test1', false)
-    expect(selected.value).toEqual(['test1'])
-  })
-
-  it('should handle selecting a value that is not already selected when deselectOnReselect is true', () => {
-    const selected = ref(['test1'])
-    const multiSelect = new MultiSelect<string>(selected)
-    multiSelect.select('test2', true)
+    multiSelect.select('test2')
     expect(selected.value).toEqual(['test1', 'test2'])
   })
 
-  it('should handle selecting a value that is not already selected when deselectOnReselect is false', () => {
-    const selected = ref(['test1'])
-    const multiSelect = new MultiSelect<string>(selected)
-    multiSelect.select('test2', false)
-    expect(selected.value).toEqual(['test1', 'test2'])
-  })
-
-  it('should handle deselecting a value that is already selected', () => {
+  it('should deselect a value', () => {
     const selected = ref(['test1', 'test2'])
     const multiSelect = new MultiSelect<string>(selected)
     multiSelect.deselect('test1')
+    multiSelect.deselect('test3')
     expect(selected.value).toEqual(['test2'])
   })
 
-  it('should handle deselecting a value that is not already selected', () => {
+  it('should deselect an already selected value', () => {
     const selected = ref(['test1', 'test2'])
     const multiSelect = new MultiSelect<string>(selected)
-    multiSelect.deselect('test3')
-    expect(selected.value).toEqual(['test1', 'test2'])
+    multiSelect.select('test1')
+    expect(selected.value).toEqual(['test2'])
+  })
+
+  it('should check if a value is selected', () => {
+    const selected = ref(['test1', 'test2'])
+    const multiSelect = new MultiSelect<string>(selected)
+
+    expect(multiSelect.isSelected('test1')).toBe(true)
+    expect(multiSelect.isSelected('test3')).toBe(false)
   })
 })
 
@@ -98,61 +86,54 @@ describe('MultiSelect', () => {
 describe('SelectionGroup', () => {
   it('can be instantiated', () => {
     const value = []
-    const group = new SelectionGroup(ref(value))
+    const group = useSelectionGroup(ref(value))
     expect(group).toBeDefined()
   })
 
   it('can be instantiated with default value', () => {
     const value = 1
-    const group = new SelectionGroup(ref([value]))
+    const group = useSelectionGroup(ref([value]))
     expect(group.selected.value).toEqual([value])
   })
 
   it('should select a value', () => {
     const value = 1
-    const group = new SelectionGroup(ref<number[]>([]), {})
-    group.select(value)
-    expect(group.selected.value).toEqual([value])
-  })
-
-  it('should select a value', () => {
-    const value = 1
-    const group = new SelectionGroup(ref<number[]>([]))
+    const group = useSelectionGroup<number>(ref([]), {})
     group.select(value)
     expect(group.selected.value).toEqual([value])
   })
 
   it('should clear selected values', () => {
     const value = 1
-    const group = new SelectionGroup(ref([value]))
+    const group = useSelectionGroup(ref([value]))
     group.clearSelected()
     expect(group.selected.value).toEqual([])
   })
 
-  it('should deselect a value if it is selected and deselectOnReselect is true', () => {
+  it('should deselect a value if it is selected and deselection is true', () => {
     const value = 1
-    const group = new SelectionGroup(ref([value]), { deselection: () => true })
+    const group = useSelectionGroup(ref([value]), { deselection: () => true })
     group.select(value)
     expect(group.selected.value).toEqual([])
   })
 
-  it('should not deselect a value if it is selected and deselectOnReselect is false', () => {
+  it('should not deselect a value if it is selected and deselection is false', () => {
     const value = 1
-    const group = new SelectionGroup(ref([value]), { deselection: () => false })
+    const group = useSelectionGroup(ref([value]), { deselection: () => false })
     group.select(value)
     expect(group.selected.value).toEqual([value])
   })
 
   it('can clear selected values even if it is already empty', () => {
     const value = 1
-    const group = new SelectionGroup(ref([value]))
+    const group = useSelectionGroup(ref([value]))
     group.clearSelected()
     expect(group.selected.value).toEqual([])
   })
 
   it('should deselect a value', () => {
     const value = 1
-    const group = new SelectionGroup(ref([value]))
+    const group = useSelectionGroup(ref([value]))
     group.deselect(value)
     expect(group.selected.value).toEqual([])
   })
--- src/composables/selection-group.ts
+++ src/composables/selection-group.ts
@@ -1,50 +1,59 @@
+import type { Getter } from '@/types'
 import { watch, type Ref } from 'vue'
 
 type PrimitiveValue = string | number | boolean | symbol
 
 interface Options {
-  multiselect?: () => boolean
-  deselection?: () => boolean
+  multiselect?: Getter<boolean>
+  deselection?: Getter<boolean>
+}
+
+export interface SelectionGroup<T> {
+  isSelected: (value: T) => boolean
+  clearSelected: () => void
+  select: (value: T) => void
+  deselect: (value: T) => void
+  selected: Ref<T[]>
 }
 
 /**
  * Represents a selection group for managing a group of selectable values.
  */
-export class SelectionGroup<T extends PrimitiveValue> {
-  deselection = () => false
-  multiselect = () => false
-  selected: Ref<T[]>
-  private strategy: SelectionStrategy<T>
+export function useSelectionGroup<T extends PrimitiveValue>(
+  selected: Ref<T[]>,
+  options: Options = {}
+): SelectionGroup<T> {
+  const deselection = options.deselection ?? (() => false)
+  const multiselect = options.multiselect ?? (() => false)
+  let strategy = multiselect() ? new MultiSelect(selected) : new SingleSelect(selected)
 
-  constructor(selected: Ref<T[]>, options: Options = {}) {
-    const { deselection, multiselect } = options
-
-    this.selected = selected
-    deselection && (this.deselection = deselection)
-    multiselect && (this.multiselect = multiselect)
-    this.strategy = this.multiselect()
-      ? new MultiSelect(this.selected)
-      : new SingleSelect(this.selected)
-
-    watch(
-      () => this.multiselect(),
-      (multi) => {
-        this.clearSelected()
-        this.strategy = multi ? new MultiSelect(this.selected) : new SingleSelect(this.selected)
-      }
-    )
+  const select = (value: T): void => {
+    strategy.select(value, deselection())
   }
 
-  select(value: T): void {
-    this.strategy.select(value, this.deselection())
+  const deselect = (value: T): void => {
+    strategy.deselect(value)
   }
 
-  deselect(value: T): void {
-    this.strategy.deselect(value)
+  const isSelected = (value: T): boolean => {
+    return strategy.isSelected(value)
   }
 
-  clearSelected(): void {
-    this.selected.value = []
+  const clearSelected = (): void => {
+    selected.value = []
+  }
+
+  watch(multiselect, (multi) => {
+    clearSelected()
+    strategy = multi ? new MultiSelect(selected) : new SingleSelect(selected)
+  })
+
+  return {
+    select,
+    selected,
+    deselect,
+    isSelected,
+    clearSelected,
   }
 }
 
@@ -90,11 +99,9 @@ export class MultiSelect<T> extends SelectionStrategy<T> {
     this.selected.value = this.selected.value.filter((_value) => _value !== value)
   }
 
-  select(value: T, deselectOnReselect: boolean): void {
+  select(value: T): void {
     if (this.isSelected(value)) {
-      if (deselectOnReselect) {
-        this.deselect(value)
-      }
+      this.deselect(value)
     } else {
       this.selected.value = [...this.selected.value, value]
     }
